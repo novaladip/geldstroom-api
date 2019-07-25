e8e395aa2724da0a224c6bdb7b58d587a40f5252
A. Noval Adip <a.novaladip@yahoo.com>
2019-07-26 05:34:47 +0700

    add register user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Inject } from '@nestjs/common';
+import { RegisterDto } from './dto/register.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/register')
+  registerUser(@Body() registerDto: RegisterDto): Promise<void> {
+    return this.authService.registerUser(registerDto);
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserRepository } from './user.repository';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UserRepository)
+    private readonly userRepository: UserRepository,
+  ) {}
+
+  registerUser(registerDto: RegisterDto): Promise<void> {
+    return this.userRepository.registerUser(registerDto);
+  }
+}
