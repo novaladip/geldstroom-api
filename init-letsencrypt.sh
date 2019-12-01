81d3de05b0fd6a97a7b30abd9585b482f82cd08d
A. Noval Adip <a.novaladip@yahoo.com>
2019-12-01 08:53:41 +0700

    update email init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(cotcapp.my.id www.cotcapp.my.id)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
+email="a.novaladip@yahoo.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
